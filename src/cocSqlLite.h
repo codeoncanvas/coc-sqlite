8784473be701f1089ea60844702859116d4e2b0c
lukasz karluk <julapy@googlemail.com>
2016-04-29 16:33:44 +1000

    added cocSqlLite placeholder class.

---
+++ src/cocSqlLite.h
@@ -0,0 +1,20 @@
+//
+//	    ┌─┐╔═╗┌┬┐┌─┐
+//      │  ║ ║ ││├┤
+//      └─┘╚═╝─┴┘└─┘
+//	 ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+//	 │  ├─┤║║║└┐┌┘├─┤└─┐
+//	 └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+//	http://codeoncanvas.cc
+//
+// Copyright (c) 2016, Code on Canvas Pty Ltd
+//
+
+#pragma once
+
+#include "SQLiteCpp.h"
+
+namespace coc {
+
+
+}
