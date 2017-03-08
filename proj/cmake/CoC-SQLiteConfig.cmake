574df0ad27d1759aee0ce8aa2c9e404e55310e4c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-03-08 13:13:34 +1100

    update source files in cmake

--- proj/cmake/CoC-SQLiteConfig.cmake
+++ proj/cmake/CoC-SQLiteConfig.cmake
@@ -2,14 +2,21 @@ if( NOT TARGET CoC-SQLite )
     get_filename_component( COC-SQLITE_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
     get_filename_component( COC-SQLITE_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../src" ABSOLUTE )
 
-    FILE(GLOB COC-SQLITE_SOURCES ${COC-SQLITE_SOURCE_PATH}/*.cpp)
+    list( APPEND COC-SQLITE_SOURCES
+            ${COC-SQLITE_SOURCE_PATH}/cocSQLite.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/Backup.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/Column.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/Database.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/Statement.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/Transaction.cpp
+            ${COC-SQLITE_PATH}/libs/SQLiteCpp/sqlite3/sqlite3.c
+            )
 
-    FILE(GLOB COC-SQLITECPP_SOURCES ${COC-SQLITE_PATH}/libs/SQLiteCpp/src/*.cpp)
-
-    add_library( CoC-SQLite ${COC-SQLITE_SOURCES} ${COC-SQLITECPP_SOURCES} )
+    add_library( CoC-SQLite ${COC-SQLITE_SOURCES} )
 
     target_include_directories( CoC-SQLite PUBLIC
             "${COC-SQLITE_SOURCE_PATH}"
-            "${COC-SQLITE_PATH}/libs/SQLiteCpp/include" )
+            "${COC-SQLITE_PATH}/libs/SQLiteCpp/include"
+            "${COC-SQLITE_PATH}/libs/SQLiteCpp/sqlite3" )
 
 endif()
